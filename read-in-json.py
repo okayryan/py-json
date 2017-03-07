41e74acdc58bbab05e1926e867927aaa248b2d2b
okayryan <source.kohlerville@gmail.com>
2017-03-06 22:10:14 -0800

    Initial read in json script

---
+++ read-in-json.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# cobbled through by multiple stack overflows
+
+import json
+import os
+
+def read_in_json(filename):
+    try:
+        with open(filename) as json_data:
+          data = json.load(json_data)
+          return data
+    except IOError:
+        print 'Error reading in ' + filename
+        sys.exit(0)
+       
+def main():
+    try:
+        filename = 'filename.json'
+        my_data = read_in_json(filename)
+    except KeyboardInterrupt:
+        print "Shutdown requested...exiting"
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    sys.exit(0)
+        
+if __name__ == "__main__":
+    main()
