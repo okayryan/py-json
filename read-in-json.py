d70da9efd0fa28e3d04ca8076ea1ac959d22d068
okayryan <source.kohlerville@gmail.com>
2017-03-06 22:15:48 -0800

    Updated script and it works using a generic json

--- read-in-json.py
+++ read-in-json.py
@@ -2,26 +2,30 @@
 # cobbled through by multiple stack overflows
 
 import json
-import os
+import sys
+import traceback
+
 
 def read_in_json(filename):
     try:
         with open(filename) as json_data:
-          data = json.load(json_data)
-          return data
+            data = json.load(json_data)
+            return data
     except IOError:
         print 'Error reading in ' + filename
         sys.exit(0)
-       
+
+
 def main():
     try:
         filename = 'filename.json'
         my_data = read_in_json(filename)
+        print my_data
     except KeyboardInterrupt:
         print "Shutdown requested...exiting"
     except Exception:
         traceback.print_exc(file=sys.stdout)
     sys.exit(0)
-        
+
 if __name__ == "__main__":
     main()
